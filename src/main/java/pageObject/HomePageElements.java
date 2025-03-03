68cf1993f9c7a98e42dffcfbf3480fecb57d9519
Sagar Yenkar <sagaryenkar@gmail.com>
2025-03-03 16:41:41 +0530

    Fixed TestNG XML file path issue

--- src/main/java/pageObject/HomePageElements.java
+++ src/main/java/pageObject/HomePageElements.java
@@ -3,6 +3,6 @@ package pageObject;
 public interface HomePageElements {
 	
 	String signInButtonText= "//span[@class='nav-line-2 ']";
-	String amzonLogo="(//a[@lang='en'])[1]";
+	String amzonLogo="//*[@id=\"nav-logo-sprites\"]";
 
 }
