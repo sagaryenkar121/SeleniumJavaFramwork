21bb7466d1de81948fdf57802c62107975b6866e
Sagar Yenkar <sagaryenkar@gmail.com>
2025-03-04 00:52:32 +0530

    Fixed Xpath and update test case

--- src/main/java/pageEvents/LoginPageEvents.java
+++ src/main/java/pageEvents/LoginPageEvents.java
@@ -24,7 +24,13 @@ public class LoginPageEvents {
 	WebDriver driver;
 
 	public void verifyIfLoginPageIsLoaded() {
-		Assert.assertTrue(ele.getWebElements("XPATH", LoginPageElements.continueBtn).size() > 0, "Element not found");
+		//Assert.assertTrue(ele.getWebElements("XPATH", LoginPageElements.continueBtn).size() > 0, "Element not found");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		boolean isElementPresent = wait.until(driver -> 
+		        ele.getWebElements("XPATH", LoginPageElements.continueBtn).size() > 0);
+
+		Assert.assertTrue(isElementPresent, "❌ Element not found!");
+		System.out.println("✅ Continue button is present on the login page.");
 
 	}
 
@@ -32,9 +38,9 @@ public class LoginPageEvents {
 
 		Thread.sleep(3000);
 		if(ele.getWebElement("XPATH", HomePageElements.AmzonLogoBeforeLogin).isDisplayed()) {
-			System.out.println("Amazon.sg logo is displayed correctly Before Login.");
+			System.out.println("✅Amazon.sg logo is displayed correctly Before Login.");
 		}else {
-			 System.out.println("Amazon.sg logo is NOT displayed! BeforeLogin");
+			 System.out.println("✅ Amazon.sg logo is NOT displayed! BeforeLogin");
 			 
 		}
 		//Thread.sleep(5000);
@@ -48,19 +54,19 @@ public class LoginPageEvents {
 		ele.getWebElement("XPATH", LoginPageElements.submitPassword).click();
 		Thread.sleep(3000);
 		if (ele.getWebElement("XPATH", HomePageElements.amzonLogo).isDisplayed()) {
-			System.out.println("Amazon.sg logo is displayed correctly After Login.");
+			System.out.println("✅ Amazon.sg logo is displayed correctly After Login.");
 		} else {
-			System.out.println("Amazon.sg logo is NOT displayed! After Login");
+			System.out.println("✅ Amazon.sg logo is NOT displayed! After Login");
 			
 		}
 		Thread.sleep(3000);
 		Assert.assertTrue(ele.getWebElement("XPATH", HomePageElements.amzonLogo).isDisplayed(),
-				"Amazon logo is NOT displayed! Before Login");
+				"✅ Amazon logo is NOT displayed! Before Login");
 
 		String actualText = ele.getWebElements("XPATH", LoginPageElements.hellowUser).get(0).getText().trim();
 		System.out.println("Actual Greeting Text: " + actualText);
 		Assert.assertTrue(actualText.toLowerCase().contains("hello"),
-				"Login greeting text does not contain 'Hello'. Actual: " + actualText);
+				"Login greeting text does not contain '✅ Hello'. Actual: " + actualText);
 
 	}
 
@@ -87,19 +93,19 @@ public class LoginPageEvents {
 
 		WebElement cartIcon = ele.getWebElement("XPATH", LoginPageElements.cartIcon);
 		Assert.assertTrue(cartIcon.isDisplayed(), "Cart icon is missing after login!");
-		System.out.println("Cart icon is displayed correctly after login.");
+		System.out.println("✅ Cart icon is displayed correctly after login.");
 
 // Verify Retrun Order Icon
 
 		WebElement RetrungOrder = ele.getWebElement("XPATH", LoginPageElements.returnOrder);
 		Assert.assertTrue(RetrungOrder.isDisplayed(), "Cart icon is missing after login!");
-		System.out.println("Return And Order  icon is displayed correctly after login.");
+		System.out.println("✅ Return And Order  icon is displayed correctly after login.");
 
 // Verify Navigation Bar
         Thread.sleep(3000);
 		WebElement NavBar = ele.getWebElement("XPATH", LoginPageElements.NavBar);
 		Assert.assertTrue(NavBar.isDisplayed(), "Navigation Bar is missing after login!");
-		System.out.println("Navigation is displayed correctly after login.");
+		System.out.println("✅ Navigation is displayed correctly after login.");
 
 	}
 
