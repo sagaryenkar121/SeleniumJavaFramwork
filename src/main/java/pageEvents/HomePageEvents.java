6c1b4915f73b97c0b8c5edf36cebf3a8352c3616
Sagar Yenkar <sagaryenkar@gmail.com>
2025-03-03 17:35:44 +0530

    Fixed Xpath

--- src/main/java/pageEvents/HomePageEvents.java
+++ src/main/java/pageEvents/HomePageEvents.java
@@ -6,14 +6,14 @@ import utils.ElementFetch;
 public class HomePageEvents {
 	ElementFetch ele= new ElementFetch();
 	public void signInButton() throws InterruptedException {
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		if(ele.getWebElement("XPATH", HomePageElements.amzonLogo).isDisplayed()) {
 			System.out.println("Amazon.sg logo is displayed correctly Before Login.");
 		}else {
 			 System.out.println("Amazon.sg logo is NOT displayed! BeforeLogin");
 			 
 		}
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		Assert.assertTrue(ele.getWebElement("XPATH", HomePageElements.amzonLogo).isDisplayed(), "Amazon logo is NOT displayed! Before Login");
 		
 		ele.getWebElement("XPATH", HomePageElements.signInButtonText).click();
